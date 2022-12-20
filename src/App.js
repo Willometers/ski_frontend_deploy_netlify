990d5f96145b3a9b6a04c734a0f5458784907dbd
BillyMiles <williammiles201@gmail.com>
2022-12-20 13:25:21 -0500

    browser router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <NavBar />
-    </div>
+    <Router>
+
+        <h1>Hello</h1>
+        <NavBar />
+        
+    </Router>
   );
 }
 
