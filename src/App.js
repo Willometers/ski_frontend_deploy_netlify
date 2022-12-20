bb8f4d43dd90f892fe4cde2687c1f34b136ea71e
BillyMiles <williammiles201@gmail.com>
2022-12-20 13:44:40 -0500

    browser router curtailed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <Router>
-
+      <div>
         <h1>Hello</h1>
         <NavBar />
-        
+        </div>
     </Router>
   );
 }
