af67cb0c6f493591ccaca08a112b54fef42c6142
BillyMiles <williammiles201@gmail.com>
2022-12-20 19:02:07 -0500

    re-added fetch and creds

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -8,8 +8,6 @@ const WeatherShow = () => {
     const options = {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
             'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
             'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
         }
@@ -17,8 +15,9 @@ const WeatherShow = () => {
 
     useEffect(() => {
         fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
     })
 
         // console.log(weather)
