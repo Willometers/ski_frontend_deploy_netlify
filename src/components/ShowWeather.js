dd6b1d0e1433f2ec421b487537619dfcec3e65bc
BillyMiles <williammiles201@gmail.com>
2022-12-22 14:08:50 -0500

    turned off fetch

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -5,22 +5,22 @@ const WeatherShow = () => {
     
     const [weather, setWeather ] = useState([])
 
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
-                'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
-            }
-        };
+    // useEffect(() => {
+    //     const options = {
+    //         method: 'GET',
+    //         headers: {
+    //             'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
+    //             'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+    //         }
+    //     };
 
-        fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
-            .then(response => response.json())
-            .then(response => setWeather(response))
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
-            console.log(weather)
-    })
+    //     fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
+    //         .then(response => response.json())
+    //         .then(response => setWeather(response))
+    //         .then(response => console.log(response))
+    //         .catch(err => console.error(err));
+    //         console.log(weather)
+    // })
 
         
 
