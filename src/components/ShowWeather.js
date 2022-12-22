d26f09b974f77e2ca47381ae2e583057565dd2f5
BillyMiles <williammiles201@gmail.com>
2022-12-22 14:07:42 -0500

    useffect added to weather page

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const WeatherShow = () => {  
     
     const [weather, setWeather ] = useState([])
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
-            'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
-        }
-    };
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
+                'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+            }
+        };
 
-    fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
-        .then(response => response.json())
-        .then(response => setWeather(response))
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
+            .then(response => response.json())
+            .then(response => setWeather(response))
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+            console.log(weather)
+    })
 
-        console.log(weather)
+        
 
     //  if (weather.length > 0)
         return (
