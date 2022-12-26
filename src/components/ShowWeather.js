36630f16abe8dae286a95fa19c21fbeb8476e328
BillyMiles <williammiles201@gmail.com>
2022-12-26 11:20:10 -0500

    weatherAPI key added to env

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -6,19 +6,9 @@ const WeatherShow = () => {
     const [weather, setWeather ] = useState([])
 
     useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'b308c25a70msh6435785816fb0dep1bf6dejsn6078fe51dd14',
-                'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
-            }
-        };
-
-        fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
+        fetch()
             .then(response => response.json())
             .then(response => setWeather(response))
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
     }, [])
 
         
