fa142e2249e42c474b00f6ba8682e210e0c5eb09
BillyMiles <williammiles201@gmail.com>
2022-12-26 11:46:10 -0500

    cors header taken out

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -6,7 +6,7 @@ const WeatherShow = () => {
     const [weather, setWeather ] = useState([])
 
     useEffect(() => {
-        fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERKey}&q=13066&aqi=no`, {mode: "no-cors"})
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERKey}&q=13066&aqi=no`)
             .then(res => res.json())
             .then(res => console.log(res))
             .then(res => setWeather(res))
