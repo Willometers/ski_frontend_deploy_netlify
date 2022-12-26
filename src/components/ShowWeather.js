d9b4de7c86479a5bb7ed0f8284353d9961130311
BillyMiles <williammiles201@gmail.com>
2022-12-26 11:37:41 -0500

    key typo fixed

--- src/components/ShowWeather.js
+++ src/components/ShowWeather.js
@@ -6,7 +6,7 @@ const WeatherShow = () => {
     const [weather, setWeather ] = useState([])
 
     useEffect(() => {
-        fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERKey}}&q=13066&aqi=no`)
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHERKey}&q=13066&aqi=no`)
             .then(res => res.json())
             .then(res => setWeather(res))
             .then(res => console.log(res))
